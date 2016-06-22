66bb16a3c90cdd6b6b2382e9e49dbfd000c9c209
cockscomb <cockscomb@hatena.ne.jp>
2016-06-22 18:10:49 +0900

    Add support Swift 3.0

--- ArcTextView.playground/Contents.swift
+++ ArcTextView.playground/Contents.swift
@@ -1,18 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-import XCPlayground
+import PlaygroundSupport
 
 let str = "Hello, playground"
 
 let view = ArcTextView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
-view.backgroundColor = UIColor.whiteColor()
-view.attributedText = NSAttributedString(string: str, attributes: [
-    NSForegroundColorAttributeName: UIColor.blackColor(),
-    NSFontAttributeName: UIFont.systemFontOfSize(24),
-])
-view.alignment = .Center
+view.backgroundColor = UIColor.white()
+view.attributedText = AttributedString(string: str, attributes: [
+    NSForegroundColorAttributeName: UIColor.black(),
+    NSFontAttributeName: UIFont.systemFont(ofSize: 24),
+    ])
+view.alignment = .center
 view.radius = 100
 view.angle = 2 * CGFloat(M_PI)
 
-XCPlaygroundPage.currentPage.liveView = view
+PlaygroundPage.current.liveView = view
